9d2074ec48c780c7336ebb81f6cc8841ffe79d2c
Bulat <babdullatypov@gmail.com>
2025-04-25 14:13:33 +0300

    Spentenergy: implement MeanSpeed function

--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -23,8 +23,13 @@ func RunningSpentCalories(steps int, weight, height float64, duration time.Durat
 }
 
 func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
-	// TODO: реализовать функцию
-	return 0
+	if duration <= 0 {
+		return 0
+	}
+	if steps < 0 {
+		return 0
+	}
+	return Distance(steps, height) / duration.Hours()
 }
 
 func Distance(steps int, height float64) float64 {
