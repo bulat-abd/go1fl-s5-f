e9124b2654b5980f0daebff9fa4f915d33a49f8c
Bulat <babdullatypov@gmail.com>
2025-04-25 15:14:10 +0300

    Daysteps: implement Parse mathod for DaySteps struct

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,6 +1,9 @@
 package daysteps
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/Yandex-Practicum/tracker/internal/personaldata"
@@ -13,7 +16,26 @@ type DaySteps struct {
 }
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+	fields := strings.Split(datastring, ",")
+	if len(fields) != 2 {
+		return fmt.Errorf("bad format: exactly one comma must be present, separating exactly two fields")
+	}
+	steps, err := strconv.Atoi(fields[0])
+	if err != nil {
+		fmt.Errorf("bad format: could not parse int in the first field - %w", err)
+	}
+	if steps <= 0 {
+		return fmt.Errorf("bad data: step count must be positive")
+	}
+	duration, err := time.ParseDuration(fields[1])
+	if err != nil {
+		return fmt.Errorf("bad format: could not parse duration - %w", err)
+	}
+	if duration <= 0 {
+		return fmt.Errorf("bad data: duration must be positive")
+	}
+	ds.Steps = steps
+	ds.Duration = duration
 	return nil
 }
 
