a1f883b584b8814f4126a9360d9ec8ed21742602
Bulat <babdullatypov@gmail.com>
2025-04-25 14:50:09 +0300

    Trainings: implement Parse method for Training struct

--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -1,8 +1,12 @@
 package trainings
 
 import (
-	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
 )
 
 type Training struct {
@@ -13,7 +17,28 @@ type Training struct {
 }
 
 func (t *Training) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+	fields := strings.Split(datastring, ",")
+	if len(fields) != 3 {
+		return fmt.Errorf("bad format: there must be exactly two commas separating three fields")
+	}
+	activity := fields[1]
+	steps, err := strconv.Atoi(fields[0])
+	if err != nil {
+		return fmt.Errorf("bad format: could not parse int in first field - %w", err)
+	}
+	if steps <= 0 {
+		return fmt.Errorf("bad data: step count must be positive")
+	}
+	duration, err := time.ParseDuration(fields[2])
+	if err != nil {
+		return fmt.Errorf("bad format: could not parse duration in third field - %w", err)
+	}
+	if duration <= 0 {
+		return fmt.Errorf("bad data: duration must be positive")
+	}
+	t.Steps = steps
+	t.TrainingType = activity
+	t.Duration = duration
 	return nil
 }
 
