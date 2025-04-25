5e225b36f97dea4df68e4143453e04f52ff542e3
Bulat <babdullatypov@gmail.com>
2025-04-25 15:46:17 +0300

    Actioninfo: implement Info function

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,5 +1,10 @@
 package actioninfo
 
+import (
+	"fmt"
+	"log"
+)
+
 type DataParser interface {
 	Parse(string) error
 	ActionInfo() (string, error)
@@ -7,4 +12,17 @@ type DataParser interface {
 
 func Info(dataset []string, dp DataParser) {
 	// TODO: реализовать функцию
+	for _, datum := range dataset {
+		err := dp.Parse(datum)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		result, err := dp.ActionInfo()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		fmt.Println(result)
+	}
 }
