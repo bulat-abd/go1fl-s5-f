dc89a818e239abaa055d0a700ec997b4069aa197
Bulat <babdullatypov@gmail.com>
2025-05-02 12:21:45 +0300

    Personaldata: build info string in one take

--- internal/personaldata/personaldata.go
+++ internal/personaldata/personaldata.go
@@ -10,7 +10,6 @@ type Personal struct {
 
 func (p Personal) Print() {
 	// TODO: реализовать функцию
-	fmt.Printf("Имя: %s\n", p.Name)
-	fmt.Printf("Вес: %.2f кг.\n", p.Weight)
-	fmt.Printf("Рост: %.2f м.\n\n", p.Height)
+	info := fmt.Sprintf("Имя: %s\nВес: %.2f кг.\nРост: %.2f м.\n\n", p.Name, p.Weight, p.Height)
+	fmt.Print(info)
 }
